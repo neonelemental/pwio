73b5136fbed68c52e92cb6ce39f95f097e448ed4
neonelemental <mikekolich@gmail.com>
2020-02-02 16:27:54 -0500

    Adds Website model

---
+++ db/migrate/20200202212732_create_websites.rb
@@ -0,0 +1,9 @@
+class CreateWebsites < ActiveRecord::Migration[6.0]
+  def change
+    create_table :websites do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/websites.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :website do
+    name { "MyString" }
+  end
+end
